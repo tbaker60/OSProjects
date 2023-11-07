7c83fd5d4db5de3338ab3744d1a5b0374ae4c614
Toby <131214504+tbaker60@users.noreply.github.com>
2023-11-07 17:55:16 -0500

    Update producer.c

--- producer.c
+++ producer.c
@@ -42,23 +42,3 @@ void* producer(void* arg) {
 
    pthread_exit(NULL);
 }
-
-int main(){
-    pthread_t producerThread, consumerThread;
-    int item = 1;
-    sem_init(&mutex, 1, 1);
-    sem_init(&full, 1, 0);
-    sem_init(&empty, 1, BUFFER_SIZE);
-
-    pthread_create(&producerThread, NULL, producer, NULL);
-    pthread_create(&consumerThread, NULL, consumer, NULL);
-
-    pthread_join(&producerThread, NULL);
-    pthread_join(&consumerThread, NULL);
-
-    sem_destroy(&mutex);
-    sem_destroy(&full);
-    sem_destroy(&empty);
-
-    return 0;
-}
