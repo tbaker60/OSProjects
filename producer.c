9f73c69b71d10f60ea64441a5723db23debe1004
Toby <131214504+tbaker60@users.noreply.github.com>
2023-11-07 15:15:26 -0500

    Update producer.c

--- producer.c
+++ producer.c
@@ -1,8 +1,27 @@
 // Toby Baker
 // OS Proj 1 producer file
-#include <consumer.c>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define BUFFER_SIZE 5
+#define MAX_ITEMS 20
+
+// MAKE SURE YOU ADD COMMENTS TO THIS CODE
+
+int buffer[BUFFER_SIZE];
+int in = 0;
+int out = 0;
+int produced_count = 0;
+int consumed_count = 0;
+
+sem_t mutex;
+sem_t full;
+sem_t empty;
 
 int main(){
-
+    int item = 1;
+    sem_init (&mutex, )
 
 }
