75266462797abde34ce013d534094cb840b477ce
Toby <131214504+tbaker60@users.noreply.github.com>
2023-11-07 17:28:08 -0500

    updating producer file

--- producer.c
+++ producer.c
@@ -20,8 +20,42 @@ sem_t mutex;
 sem_t full;
 sem_t empty;
 
+void* producer(void* arg) {
+   int item = 1;
+
+   while (produced_count < MAX_ITEMS) {
+      sem_wait(&empty);
+      sem_wait(&mutex);
+
+      buffer[in] = item;
+      printf("Produced: %d", item);
+      item++;
+      in = (in + 1) % BUFFER_SIZE;
+
+      produced_count++;
+
+      sem_post(&mutex);
+      sem_post(&full);
+   }
+
+   pthread_exit(NULL);
+}
+
 int main(){
     int item = 1;
-    sem_init (&mutex, )
+    sem_init(&mutex, 1, 1);
+    sem_init(&full, 1, 0);
+    sem_init(&empty, 1, BUFFER_SIZE);
 
+    pthread_create(&producerThread, NULL, producer, NULL);
+    pthread_create(&consumerThread, NULL, consumer, NULL);
+
+    pthread_join(&producerThread, NULL);
+    pthread_join(&consumerThread, NULL);
+
+    sem_destroy(&mutex);
+    sem_destroy(&full);
+    sem_destroy(&empty);
+
+    return 0;
 }
