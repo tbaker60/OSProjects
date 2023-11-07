2c5aab4a78403ce62d41a22faee3753391a09768
Toby <131214504+tbaker60@users.noreply.github.com>
2023-11-07 17:36:37 -0500

    updated consumer.c

--- consumer.c
+++ consumer.c
@@ -5,6 +5,22 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-int main() {
+#define BUFFER_SIZE 5
+#define MAX_ITEMS 20
 
+void* consumer(void* arg) {
+    while(consumed_count < MAX_ITEMS) {
+        sem_wait(&full);
+        sem_wait(&empty);
+
+        int item = buffer[out];
+        printf("Consumed: %d", item);
+        out = (out + 1) % BUFFER_SIZE;
+        ++consumed_count;
+
+        sem_post(&mutex);
+        sem_post(&empty);
+    }
+
+    pthread_exit(NULL);
 }
