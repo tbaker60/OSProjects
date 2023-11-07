dcf495c1c86226b3dd5fd95608fb46f92e0f3e30
Toby <131214504+tbaker60@users.noreply.github.com>
2023-11-07 17:55:39 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,2 +1,23 @@
 // Toby Baker
 // OS Project 1 main file
+
+
+int main(){
+    pthread_t producerThread, consumerThread;
+    int item = 1;
+    sem_init(&mutex, 1, 1);
+    sem_init(&full, 1, 0);
+    sem_init(&empty, 1, BUFFER_SIZE);
+
+    pthread_create(&producerThread, NULL, producer, NULL);
+    pthread_create(&consumerThread, NULL, consumer, NULL);
+
+    pthread_join(&producerThread, NULL);
+    pthread_join(&consumerThread, NULL);
+
+    sem_destroy(&mutex);
+    sem_destroy(&full);
+    sem_destroy(&empty);
+
+    return 0;
+}
