cdebe41cd6218dec72e4b409535e961ac013965e
Toby <131214504+tbaker60@users.noreply.github.com>
2023-11-30 14:56:53 -0500

    Create structs.h

---
+++ Project2/structs.h
@@ -0,0 +1,12 @@
+struct Process {
+// Arrays for storing allocation values
+// first index is allocations for A, second is B, etc.
+  int Alloc[3];
+  int maxAllox[3];
+  
+}
+
+struct Resource {
+  int count;
+  int max;
+}
