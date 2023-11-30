a6bb18157a5f935ee840bfa5171ecea37139a19d
Toby <131214504+tbaker60@users.noreply.github.com>
2023-11-30 15:02:24 -0500

    Update bankersAlgorithm.c

--- Project2/bankersAlgorithm.c
+++ Project2/bankersAlgorithm.c
@@ -2,12 +2,18 @@
 FILE *fptr;
 
 int main() {
-
+  // Will be used to keep track of available resources as they are read in 
+  int availResources[3];
+  
+  // Opening up file and reading data
   fptr = fopen("inputData.txt", "r");
   char fileArr[100];
   fgets(fileArr, 100, fptr);
   printf("Input Data: ", "%s", fileArr);
   fclose(fptr);
+
+  // Everything else
+ 
   printf("Safe sequence: ");
   return;
 }
