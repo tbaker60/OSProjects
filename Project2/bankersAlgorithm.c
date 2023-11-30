0e4fc9884f4163a6b18692945eb2d86890409178
Toby <131214504+tbaker60@users.noreply.github.com>
2023-11-30 16:40:19 -0500

    Update bankersAlgorithm.c

--- Project2/bankersAlgorithm.c
+++ Project2/bankersAlgorithm.c
@@ -29,9 +29,9 @@ int main() {
   }
   fclose(fptr);
 
-  printf("%i", availResources[0]); printf("/n");
-  printf("%i", availResources[1]); printf("/n");
-  printf("%i", availResources[2]); printf("/n");
+  printf("%i", availResources[0]); printf("/");
+  printf("%i", availResources[1]); printf("/");
+  printf("%i", availResources[2]); printf("/");
   // Everything else
  
   printf("Safe sequence: ");
