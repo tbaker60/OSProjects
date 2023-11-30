287d6f2275cc05f366a5903846a37c67ce9965d1
Toby <131214504+tbaker60@users.noreply.github.com>
2023-11-30 17:21:30 -0500

    adding comments

--- Project2/bankersAlgorithm.c
+++ Project2/bankersAlgorithm.c
@@ -4,6 +4,7 @@ FILE *fptr;
 int main() {
   // Will be used to keep track of available resources as they are read in 
   int availResources[3] = {10,5,7};
+  // Used to keep track of process's resource allocation
   struct Process processes[5]; 
 
   // Opening up file and reading data
@@ -18,12 +19,14 @@ int main() {
     int i = 0;  // tracks index of character array for this line
     int j = 0;  // tracks number of integers read in
     while (j < 6){
-      printf("\n");
+      //printf("\n");
       if (j<3 && fileArr[i]>= 48) {
         processes[cnt].alloc[j] = (fileArr[i]-48);
         availResources[j] -= (fileArr[i]-48);
+        /* // Testing for allocation assignments to processes
         printf("%i", (fileArr[i]-48)); 
         printf("  ");
+        */
       }
       else processes[cnt].maxAlloc[j-3];
       if (fileArr[i] == ',' || fileArr[i] == ';') ++j;
@@ -31,10 +34,11 @@ int main() {
     }
   }
   fclose(fptr);
-
+  /* Testing available resources counting
   printf("%i", availResources[0]); printf("/");
   printf("%i", availResources[1]); printf("/");
   printf("%i", availResources[2]); printf("\n");
+  */
   // Everything else
  
   printf("Safe sequence: ");
