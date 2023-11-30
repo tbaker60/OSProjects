7c12e3725926920d92971f21f9241cfb5eaf3e7d
Toby <131214504+tbaker60@users.noreply.github.com>
2023-11-30 14:47:52 -0500

    Update bankersAlgorithm.c

--- Project2/bankersAlgorithm.c
+++ Project2/bankersAlgorithm.c
@@ -1,4 +1,12 @@
+FILE *fptr;
+
 int main() {
+
+  fptr = fopen("inputData.txt", "r");
+  char fileArr[100];
+  fgets(fileArr, 100, fptr);
+  printf("Input Data: ", "%s", fileArr);
+  fclose(fptr);
   printf("Safe sequence: ");
   return;
 }
